3e921df917182641bdd6c4178827057d66064f2f
Magik3a <svetlin.krastanov90@gmail.com>
2015-01-30 20:35:04 +0200

    HomeWork6

---
+++ 01.NumbersFrom1ToN/NumbersFrom1ToN.cs
@@ -0,0 +1,24 @@
+﻿/* Problem 1. Numbers from 1 to N
+ * Write a program that enters from the console
+ * a positive integer n and prints all the numbers from
+ * 1 to n, on a single line, separated by a space */
+
+using System;
+
+class NumbersFrom1ToN
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Numbers from 1 to N";
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.SetWindowSize(80, 10);
+        Console.Write("Enter positive integer: ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= n; i++)
+        {
+            Console.Write(" " + i);
+        }
+        Console.WriteLine("\n");
+    }
+}
+
---
+++ 02.NumbersNotDivisibleBy3and7/NumbersNotDivisibleBy3and7.cs
@@ -0,0 +1,32 @@
+﻿/* Problem 2. Numbers Not Divisible by 3 and 7
+ * Write a program that enters from the console a positive
+ * integer n and prints all the numbers from 1 to n
+ * not divisible by 3 and 7, on a single line, separated by a space. */
+
+
+using System;
+
+class NumbersNotDivisibleBy3and7
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Numbers Not Divisible by 3 and 7";
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.SetWindowSize(80, 10);
+        Console.WriteLine("Enter a positive integer: ");
+        int number = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= number; i++)
+        {
+            if ((i % 3 == 0) || (i % 7 == 0))
+            {
+                Console.Write("");
+            }
+            else
+            {
+                Console.Write(" " + i);
+            }
+        }
+        Console.WriteLine("\n");
+    }
+}
+
