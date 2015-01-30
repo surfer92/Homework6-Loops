82fce0aedeb9b951ef7541b8c204d4827f51340a
Magik3a <svetlin.krastanov90@gmail.com>
2015-01-31 00:02:02 +0200

    Homework5

--- 06.CalculateNK/CalculateNK.cs
+++ 06.CalculateNK/CalculateNK.cs
@@ -8,7 +8,19 @@ class CalculateNK
 {
     static void Main(string[] args)
     {
-
+        Console.Title = "Calculate N! / K!";
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.SetWindowSize(80, 20);
+        Console.Write("Write an integer: ");
+        int n = Int32.Parse(Console.ReadLine());
+        Console.Write("Write an integer: ");
+        int k = Int32.Parse(Console.ReadLine());
+        int result = 1;
+        for (int i = n; i > k; i--)
+        {
+            result *= i;
+        }
+        Console.WriteLine(result);
     }
 }
 
